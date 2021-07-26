cee07d8065e5791d8facbe3c2fe09f5659e0d73e
nguyenphuochai1998 <haito1998@gmail.com>
2021-07-26 11:11:27 +0700

    update_test_update_data_by_provider

--- test/consumer_test.dart
+++ test/consumer_test.dart
@@ -13,23 +13,58 @@ const valueString3rd = 'provider_vn';
 main() {
   testWidgets("Consumer: basic usage", (WidgetTester tester) async {
     // provider cho key 1
-    var simpleProvider = ProviderController(
-        providers: [
-          Provider<String?>(providerKey: providerKey, value: valueString)
-        ],
-        builderChild: () {
-          return Consumer(
-              providerKey: providerKey,
-              builder: (context, value) {
-                return Text(
-                  value.toString(),
-                  textDirection: TextDirection.ltr,
-                );
-              });
-        });
+    var simpleProvider = MaterialApp(
+      home: ProviderController(
+          providers: [
+            Provider<String?>(providerKey: providerKey, value: valueString)
+          ],
+          builderChild: () {
+            return Consumer(
+                providerKey: providerKey,
+                builder: (context, value) {
+                  return Column(
+                    children: [
+                      Text(
+                        value.toString(),
+                        textDirection: TextDirection.ltr,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          ProviderController.of(context)
+                              .getProviderByKey(key: providerKey)!
+                              .setValue(valueStringNext);
+                        },
+                        child: Text("button"),
+                      )
+                    ],
+                  );
+                });
+          }),
+    );
     // test lan dau
-    await tester.pumpWidget(simpleProvider);
+    await tester.pumpWidget(
+      Builder(
+        builder: (BuildContext context) {
+          return simpleProvider;
+        },
+      ),
+    );
     var valueFinder = find.text(valueString);
     expect(valueFinder, valueFinder);
+
+    final finder = find.byType(ElevatedButton);
+
+    await tester.ensureVisible(finder);
+    await tester.pump();
+
+    await tester.tap(finder);
+    await tester.pump();
+    valueFinder = find.text(valueStringNext);
+    expect(valueFinder, valueFinder);
+    // final BuildContext context = tester.element(find.byType(ProviderController));
+    // await ProviderController.of(context).getProviderByKey(key: providerKey)!.setValue(valueStringNext);
+    // await tester.pump();
+    // valueFinder = find.text(valueStringNext);
+    // expect(valueFinder, valueFinder);
   });
 }
