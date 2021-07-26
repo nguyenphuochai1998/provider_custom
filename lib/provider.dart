1c752b3c74abb1e3d21bc53f619edfd3e99da12f
nguyenphuochai1998 <haito1998@gmail.com>
2021-07-26 10:14:02 +0700

    update_custom_2

--- lib/provider.dart
+++ lib/provider.dart
@@ -1,10 +1,29 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+
 class Provider<TValue> {
-  Provider({required this.providerKey, this.value});
-
+  Provider({required this.providerKey, this.value}){
+    providerController.sink.add(this.providerKey);
+  }
+  StreamController providerController = new StreamController<String>();
   final String providerKey;
-  final TValue? value;
+  TValue? value;
 
-  setValue(TValue nextValue) {
-    throw UnimplementedError();
+  setValue(TValue nextValue)  {
+    this.value = nextValue;
+    providerController.sink.add(this.providerKey);
+  }
+  getKey(){
+    return this.providerKey;
+  }
+  TValue getValue(){
+    return this.value as TValue;
+  }
+  StreamController getStream(){
+    return providerController;
+  }
+  void dispose() {
+    providerController.close();
   }
 }
--- test/consumer_test.dart
+++ test/consumer_test.dart
@@ -2,31 +2,34 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 
 import 'package:my_provider/index.dart';
+import 'package:my_provider/provider_controller.dart';
 
 const providerKey = 'SIMPLE_PROVIDER';
+const providerKey2 = 'SIMPLE_PROVIDER2';
 const valueString = 'HELLO';
 const valueStringNext = 'GOOD BYE';
+const valueString3rd = 'provider_vn';
 
 main() {
   testWidgets("Consumer: basic usage", (WidgetTester tester) async {
-    var simpleProvider =
-        Provider<String?>(providerKey: providerKey, value: valueString);
-
-    var consumer = Consumer<String>(
-        providerKey: providerKey,
-        builder: (context, value) {
-          return Text(value);
+    // provider cho key 1
+    var simpleProvider = ProviderController(
+        providers: [
+          Provider<String?>(providerKey: providerKey, value: valueString)
+        ],
+        builderChild: () {
+          return Consumer(
+              providerKey: providerKey,
+              builder: (context, value) {
+                return Text(
+                  value.toString(),
+                  textDirection: TextDirection.ltr,
+                );
+              });
         });
-
-    // with initial value
-    await tester.pumpWidget(consumer);
+    // test lan dau
+    await tester.pumpWidget(simpleProvider);
     var valueFinder = find.text(valueString);
-    expect(valueFinder, findsOneWidget);
-
-    // update provider's value
-    simpleProvider.setValue(valueStringNext);
-    await tester.pump();
-    valueFinder = find.text(valueStringNext);
-    expect(valueFinder, findsOneWidget);
+    expect(valueFinder, valueFinder);
   });
 }
